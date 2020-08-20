3a2c7f1b84772cd61f6959fd277800d998c0d123
jin <1542029827@qq.com>
2020-08-20 16:03:11 +0800

    设置可以状态，重新绘制

--- toy-react.js
+++ toy-react.js
@@ -1,3 +1,5 @@
+const RENDER_TO_DOM = Symbol("render to dom")
+
 // html原生的标签，用js内部方法创建
 class ElementWrapper {
     constructor(type) {
@@ -5,11 +7,31 @@ class ElementWrapper {
     }
 
     setAttribute(name, value) {
-        this.root.setAttribute(name, value)
+        if (name.match(/^on([\s\S]+)$/)) {
+            // on开头的当方法处理
+            this.root.addEventListener(RegExp.$1.replace(
+                /^[\s\S]/, c => c.toLowerCase()), value);
+        } else {
+            // 普通属性
+            if (name === "className") {
+                this.root.setAttribute("class", value);
+            } else {
+                this.root.setAttribute(name, value)
+            }
+        }
     }
 
     appendChild(component) {
-        this.root.appendChild(component.root);
+        let range = document.createRange();
+        range.setStart(this.root, this.root.childNodes.length)
+        range.setEnd(this.root, this.root.childNodes.length);
+        component[RENDER_TO_DOM](range)
+    }
+
+    // 私有渲染方法
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
     }
 }
 
@@ -17,6 +39,11 @@ class ElementWrapper {
 class TextWrapper {
     constructor(content) {
         this.root = document.createTextNode(content);
+    }
+
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root)
     }
 }
 
@@ -26,19 +53,52 @@ export class Component {
         this.props = Object.create(null);
         this.children = [];
         this._root = null;
+        this._range = null;
     }
 
     setAttribute(name, value) {
         this.props[name] = value;
     }
-    appendChild(component){
+
+    appendChild(component) {
         this.children.push(component);
     }
-    get root(){
-        if (!this._root) {
-            this._root = this.render().root;
+
+    [RENDER_TO_DOM](range) {
+        this._range = range;
+        this.render()[RENDER_TO_DOM](range)
+    }
+
+    rerender() {
+        let oldRange = this._range;
+
+        let range = document.createRange();
+        range.setStart(oldRange.startContainer, oldRange.startOffset);
+        range.setEnd(oldRange.startContainer, oldRange.startOffset);
+        this[RENDER_TO_DOM](range)
+
+        oldRange.setStart(range.endContainer, range.endOffset);
+        oldRange.deleteContents();
+    }
+
+    setState(newState) {
+        if (this.state === null || typeof this.state !== "object") {
+            this.state = newState;
+            this.rerender();
+            return;
         }
-        return this._root;
+        let merge = (oldState, newState) => {
+            for (let p in newState) {
+                if (oldState[p] === null || typeof oldState[p] !== "object") {
+                    oldState[p] = newState[p];
+                } else {
+                    merge(oldState[p], newState[p]);
+                }
+            }
+        }
+
+        merge(this.state, newState);
+        this.rerender();
     }
 }
 
@@ -62,12 +122,15 @@ export function createElement(type, attributes, ...children) {
                 // 元素的文本
                 child = new TextWrapper(child);
             }
+            if (child === null) {
+                continue;
+            }
             if (typeof child === "object" && child instanceof Array) {
                 // 自定义的元素
                 insertChildren(child);
             } else {
                 // 普通html元素，小写那种
-                e.appendChild(child)
+                e.appendChild(child);
             }
         }
     }
@@ -78,5 +141,9 @@ export function createElement(type, attributes, ...children) {
 
 // 渲染内容
 export function render(component, parentElement) {
-    parentElement.appendChild(component.root)
+    let range = document.createRange()
+    range.setStart(parentElement, 0)
+    range.setEnd(parentElement, parentElement.childNodes.length)
+    range.deleteContents()
+    component[RENDER_TO_DOM](range)
 }
